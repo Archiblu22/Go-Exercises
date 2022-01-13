92d45f1d79ccbfaab353b8b860bc8dc6b33b9056
archiblue22 <paguirre@byui.edu>
2022-01-13 13:28:36 -0700

    published through localhost

--- html/main.go
+++ html/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 //story.go
@@ -28,36 +29,107 @@ var defaultHandlerTmpl = `
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Choose Your Own Adventure</title>
+	<style>
+		body {
+			font-family: helvetica, arial;
+		}
+		h1 {
+			text-align: center;
+			position: relative;
+		}
+		.page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FFFCF6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		}
+		li {
+			padding-top: 10px;
+		}
+		a,
+		a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		}
+		a:active,
+		a:hover {
+			color: #7792a2;
+		}
+		p {
+			text-indent: 1em;
+		}
+	</style>
 </head>
 
 <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
-    <ul>
-        {{range .Options}}
-        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-        {{end}}
-    </ul>
+	<section class="page">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+		<p>{{.}}</p>
+		{{end}}
+		<ul>
+			{{range .Options}}
+			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+	</section>
 </body>
 
 </html>
 `
 
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
 }
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+func defaultPathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	return path[1:]
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	path := h.pathFn(r)
+
+	if chapter, ok := h.s[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
 	}
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
 }
 
 func JsonStory(r io.Reader) (Story, error) {
@@ -102,5 +174,5 @@ func main() {
 
 	h := NewHandler(story)
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
